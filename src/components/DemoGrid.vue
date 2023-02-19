d2d04594e489a4d40bf30a4161489e9eb83b7fb0
alirezamou <am808036@gmail.com>
2023-02-20 00:27:39 +0330

    add button event when click on, order toggle between asc & dsc

--- src/components/DemoGrid.vue
+++ src/components/DemoGrid.vue
@@ -1,31 +1,40 @@
 <script>
 export default {
-    props: {
-        data: Array,
-        columns: Array,
-        filterKey: String
-    },
-    methods: {
-        upperCaseFirstLetter (str) {
-            return str.split(" ").map(item => `${item.charAt(0).toUpperCase()}${item.slice(1)}`).join(" ");
-        }
-    },
-    computed: {
-        filteredData () {
-            let tData = this.data;
-            if(this.filterKey) {
-                const fKey = this.filterKey.toLowerCase();
-                tData = this.data.filter(row => {
-                    return Object.keys(row).some(key => {
-                        return String(row[key]).toLowerCase().indexOf(fKey) > -1
-                    });
-                });
-            }
-            // first sort array on `name` column in ascending order
-            tData.sort((a, b) => (a.name < b.name));
-            return tData;
-        }
+  data() {
+    return {
+      sortOrders: { name: 1 }
     }
+  },
+  props: {
+      data: Array,
+      columns: Array,
+      filterKey: String
+  },
+  methods: {
+      upperCaseFirstLetter (str) {
+          return str.split(" ").map(item => `${item.charAt(0).toUpperCase()}${item.slice(1)}`).join(" ");
+      }
+  },
+  computed: {
+      filteredData () {
+          let tData = this.data;
+          if(this.filterKey) {
+              const fKey = this.filterKey.toLowerCase();
+              tData = this.data.filter(row => {
+                  return Object.keys(row).some(key => {
+                      return String(row[key]).toLowerCase().indexOf(fKey) > -1
+                  });
+              });
+          }
+          // first sort array on `name` column in ascending order
+          tData = tData.slice().sort((a, b) => {
+            a = a.name;
+            b = b.name;
+            return (a === b ? 0 : a > b ? 1 : -1) * this.sortOrders.name;
+          });
+          return tData;
+      }
+  }
 }
 </script>
 
@@ -33,8 +42,8 @@ export default {
      <table class="table">
       <thead>
         <tr>
-          <th v-for="c in columns">{{ upperCaseFirstLetter(c) }}
-              <span class="arrow asc"></span>
+          <th @click="sortOrders.name *= -1" v-for="c in columns">{{ upperCaseFirstLetter(c) }}
+              <span class="arrow" :class="sortOrders.name === 1 ? 'asc' : 'dsc'"></span>
           </th>
         </tr>
       </thead>
@@ -79,6 +88,12 @@ th, td {
   border-left: 4px solid transparent;
   border-right: 4px solid transparent;
   border-bottom: 4px solid #fff;
+}
+
+.arrow.dsc {
+  border-left: 4px solid transparent;
+  border-right: 4px solid transparent;
+  border-top: 4px solid #fff;
 }
 
 </style>
