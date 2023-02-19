b932e2c1e6ca95133ae6a653b535943533d66500
alirezamou <am808036@gmail.com>
2023-02-19 12:43:50 +0330

    add DemoGrid component and move table in it

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,10 @@
 <script>
+import DemoGrid from './components/DemoGrid.vue';
+export default {
+  components: {
+    DemoGrid
+  }
+}
 </script>
 
 <template>
@@ -7,22 +13,7 @@
       <label for="">Search</label>
       <input type="text">
     </div>
-    <table class="table">
-      <thead>
-        <th>Name</th>
-        <th>Power</th>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Chuck Norris</td>
-          <td>Infinity</td>
-        </tr>
-        <tr>
-          <td>bruce lee</td>
-          <td>9000</td>
-        </tr>
-      </tbody>
-    </table>
+    <DemoGrid />
   </div>
 </template>
 
@@ -35,26 +26,5 @@
 .search-query {
   margin-bottom: 0.5rem;
 }
-
-table {
-  border: solid #42b983 2px;
-  border-radius: 2px;
-}
-
-th {
-  background-color: #42b983;
-  color: #fff;
-  font-weight: bold;
-}
-
-td {
-  background-color: #f9f9f9;
-}
-
-th, td {
-  padding: 0.5rem 2rem;
-}
-
-
 
 </style>
---
+++ src/components/DemoGrid.vue
@@ -0,0 +1,39 @@
+<template>
+     <table class="table">
+      <thead>
+        <th>Name</th>
+        <th>Power</th>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Chuck Norris</td>
+          <td>Infinity</td>
+        </tr>
+        <tr>
+          <td>bruce lee</td>
+          <td>9000</td>
+        </tr>
+      </tbody>
+    </table>
+</template>
+
+<style scoped>
+table {
+  border: solid #42b983 2px;
+  border-radius: 2px;
+}
+
+th {
+  background-color: #42b983;
+  color: #fff;
+  font-weight: bold;
+}
+
+td {
+  background-color: #f9f9f9;
+}
+
+th, td {
+  padding: 0.5rem 2rem;
+}
+</style>
