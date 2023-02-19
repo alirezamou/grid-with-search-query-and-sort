7a38e680861ca6b2c3e45b2d28743b5219d4102c
alirezamou <am808036@gmail.com>
2023-02-19 12:27:07 +0330

    implement simple ui

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,28 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <label for="">Search</label>
+    <input type="text">
+  </div>
+  <table>
+    <thead>
+      <th>Name</th>
+      <th>Power</th>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Chuck Norris</td>
+        <td>Infinity</td>
+      </tr>
+      <tr>
+        <td>bruce lee</td>
+        <td>9000</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
